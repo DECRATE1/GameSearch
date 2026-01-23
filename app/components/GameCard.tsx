f56f482ee51ecedef733c0caf00aa6d53c7e6c3f
1DECRATE1 <nukegame12@gmail.com>
2026-01-24 00:48:26 +0300

    "Изменения дизайна GameCard"

--- app/components/GameCard.tsx
+++ app/components/GameCard.tsx
@@ -1,20 +1,23 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 //Каточка игры на главной странице
 export default function GameCard({
   href,
   url,
   title,
+  release,
+  genres,
 }: {
   href: string;
   url?: string;
   title: string;
+  release: Date;
+  genres: string;
 }) {
   const [isLoad, setIsLoad] = useState(true);
   const [isOverLink, setItsOverLink] = useState(false);
-
   const handleOverLink = () => {
     setItsOverLink(!isOverLink);
   };
@@ -29,13 +32,15 @@ export default function GameCard({
 
   return (
     <Link
-      className="w-[460px] h-100 select-none text-center"
+      className="w-[460px] h-100 select-none hover:border-zinc-700 transition-all 
+      duration-300 hover:shadow-lg/80 shadow-zinc-500/50 bg-zinc-800 
+      rounded-b-xl overflow-hidden rounded-t-xl"
       href={href}
       onMouseEnter={onEnter}
       onMouseLeave={onBlur}
     >
       {isLoad ?
-        <div className="flex flex-col w-full h-full rounded-t-3xl overflow-hidden bg-[#839400] gap-2 rounded-b-xl">
+        <div className="flex flex-col w-full h-full gap-2">
           <Image
             src={`${url}`}
             alt={title}
@@ -45,14 +50,29 @@ export default function GameCard({
             onError={() => setIsLoad(false)}
             loading="lazy"
             className="w-full h-full flex items-center object-fill
-            justify-center text-center rounded-xl"
+            justify-center text-center"
             style={{
               width: url!.length > 0 ? "100%" : "80%",
             }}
           ></Image>
 
-          <div className="w-full h-30 bg-[#839400] text-white uppercase font-bold">
-            <span>{title}</span>
+          <div className="w-full h-[60%] text-white flex flex-col gap-5 px-4 text-stroke">
+            <h5>{title}</h5>
+            <ul className="[&>li]:flex [&>li]:justify-between text-lg [&>li]:text-lg [&>li]:[&>p]:text-neutral-400 [&>li]:[&>p]:text-md">
+              {genres && (
+                <>
+                  <li>
+                    Жанр: <p>{genres.split(",")[0]}</p>
+                  </li>
+                  <li>
+                    Год:{" "}
+                    <p>
+                      {release.toLocaleString().split("T").join("").slice(0, 4)}
+                    </p>
+                  </li>
+                </>
+              )}
+            </ul>
           </div>
         </div>
       : <div
